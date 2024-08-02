5b7ac4a9ad1424263c11c89bbd1023e60769ed02
Fokhruz Zaman <zfokhruz@gmail.com>
2024-08-02 14:52:52 +0600

    Initial commit

---
+++ EmployeeService.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace EmployeeService.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ PayrollService.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace PayrollService.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
